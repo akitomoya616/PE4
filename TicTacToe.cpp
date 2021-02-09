6830d1791a937ea24d05e75d9ff1b9d449a1b296
aosdnffoia <kingdra36@gmail.com>
2021-02-08 17:03:12 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -14,13 +14,33 @@ std::vector<std::vector<std::string>> CreateBoard(){
 	return board;
 }
 
-void TicTacToe::DisplayBoard(std::vector<std::vector<std::string>> board) {
+void TDisplayBoard(std::vector<std::vector<std::string>> board) {
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; col++)
             std::cout<<board[i][j]<<" ";
         std::cout << std::endl;
     }
 }
+
+Position GetPlayerChoice() {
+    int row;
+    int col;
+
+    while (1){
+        std::cout << "Enter Choice:" << std::endl;
+        std::cout << "Row: ";
+        std::cin >> row;
+        std::cout << "Col: ";
+        std::cin >> col;
+        if (board[row-1][col-1] != ⭕ && board[row-1][col-1] != ❌) {
+            Position pos{row-1,col-1};
+	    return pos;
+        }
+        else 
+            std::cout << "Invalid position!" << std::endl;
+    }
+}
+
 
 int main(){
 	std::vector<std::vector<std::string>> board=CreateBoard();
